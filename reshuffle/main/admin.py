3e2fe6b30c2dfdea404e64b8fa2a3b4defadd4cd
r4nd0lph <r11345roma@yandex.ru>
2022-12-24 23:15:05 +0300

    init repo

---
+++ reshuffle/main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+admin.site.site_title = "Reshuffle"
+admin.site.site_header = "Reshuffle"
+admin.site.index_title = "Администрирование"
---
+++ reshuffle/main/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponseNotFound
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "main/index.html", {"title": "Index"})
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("Error 404")
