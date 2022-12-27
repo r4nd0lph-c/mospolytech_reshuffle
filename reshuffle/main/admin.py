bdaaada2e32706d79851fe98575e062413b3d1cf
r4nd0lph <r11345roma@yandex.ru>
2022-12-27 19:29:17 +0300

    changed models logic; added Datestamp abstract class

--- reshuffle/main/admin.py
+++ reshuffle/main/admin.py
@@ -4,28 +4,37 @@ from django.utils.safestring import mark_safe
 from main.models import *
 
 
-@admin.register(DocsHeader)
-class DocsHeaderAdmin(admin.ModelAdmin):
-    fields = ("content", "active")
-    list_display = ("id", "pretty_content", "created", "updated", "active")
+@admin.register(DocHeader)
+class DocHeaderAdmin(admin.ModelAdmin):
+    fields = ("content", "flag")
+    list_display = ("id", "pretty_content", "created", "updated", "flag")
     list_display_links = ("id",)
     ordering = ("-updated",)
-    list_filter = ("active",)
+    list_filter = ("flag",)
 
     def pretty_content(self, obj):
         return mark_safe(f"<div style='border: 1px solid var(--hairline-color);'> {obj.content} </div>")
 
-    pretty_content.short_description = DocsHeader._meta.get_field("content").verbose_name
+    pretty_content.short_description = DocHeader._meta.get_field("content").verbose_name
 
     def delete_queryset(self, request, qs):
-        if qs.filter(active=True):
-            qs_remaining = DocsHeader.objects.order_by("-updated").exclude(pk__in=qs.values("pk"))
+        if qs.filter(flag=True):
+            qs_remaining = DocHeader.objects.order_by("-updated").exclude(pk__in=qs.values("pk"))
             if qs_remaining:
-                active_new = qs_remaining[0]
-                active_new.active = True
-                active_new.save()
+                flag_new = qs_remaining[0]
+                flag_new.flag = True
+                flag_new.save()
         qs.delete()
 
+
+# @admin.register(Subject)
+# class SubjectAdmin(admin.ModelAdmin):
+#     list_display = ("id", "title", "created", "updated", "active")
+#     list_display_links = ("id",)
+#     ordering = ("-updated",)
+#     list_filter = ("active",)
+
+# TODO: abstract class for created updated fix qs = type(self).objects.order_by("-updated") (models.py [15])
 
 admin.site.site_title = "RESHUFFLE"
 admin.site.site_header = "RESHUFFLE"
--- reshuffle/main/models.py
+++ reshuffle/main/models.py
@@ -3,44 +3,58 @@ from django.db import models
 from ckeditor.fields import RichTextField
 
 
-class SingleActiveAbstract(models.Model):
-    active = models.BooleanField(default=True, verbose_name="Актуальный")
+class AbstractDatestamp(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class AbstractSingleFlag(AbstractDatestamp):
+    flag = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
-        if self.active:
-            type(self).objects.all().update(active=False)
+        if self.flag:
+            type(self).objects.all().update(flag=False)
         super().save(*args, **kwargs)
 
     def delete(self, *args, **kwargs):
         qs = type(self).objects.order_by("-updated")
         qs = qs.exclude(pk=self.pk)
         if qs:
-            qs.filter(pk=qs[0].pk).update(active=True)
+            qs.filter(pk=qs[0].pk).update(flag=True)
         super().delete(*args, **kwargs)
 
     class Meta:
         abstract = True
 
 
-class DocsHeader(SingleActiveAbstract):
+class DocHeader(AbstractSingleFlag):
     content = RichTextField(config_name="Config_DocsHeader", verbose_name="Содержание")
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
-    updated = models.DateTimeField(auto_now=True, verbose_name="Обновлён")
 
     def __str__(self):
-        state = "Актуальный" if self.active else "Неактуальный"
+        state = "Актуальный" if self.flag else "Неактуальный"
         return f"ID: {self.id} ({state})"
 
     class Meta:
         verbose_name = "Заголовок документа"
         verbose_name_plural = "Заголовки документа"
 
-#
+
+DocHeader._meta.get_field("created").verbose_name = "Создан"
+DocHeader._meta.get_field("updated").verbose_name = "Обновлён"
+DocHeader._meta.get_field("flag").verbose_name = "Актуальный"
+
 # class Subject(models.Model):
 #     title = models.CharField(max_length=128, verbose_name="Название")
 #     instr_title = models.CharField(max_length=128, default="Инструкция по выполнению работы",
 #                                    verbose_name="Название инструкции")
 #     instr_content = models.TextField(verbose_name="Содержимое инструкции")
+#
+#     def clean(self):
+#         print("cleaned!")
+#         cleaned_data = super().clean()
 #
 #     def __str__(self):
 #         return f"{self.title}"
