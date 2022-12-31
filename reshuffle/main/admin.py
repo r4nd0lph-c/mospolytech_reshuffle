68e81822972efb4b1f9034cb57403148ea82c9bb
r4nd0lph <r11345roma@yandex.ru>
2022-12-31 21:36:00 +0300

    created models skeleton

--- reshuffle/main/admin.py
+++ reshuffle/main/admin.py
@@ -1,7 +1,77 @@
 from django.utils.translation import gettext_lazy as _
+from django.utils.safestring import mark_safe
 from django.contrib import admin
 
 from main.models import *
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Difficulty)
+class DifficultyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DocHeader)
+class DocHeaderAdmin(admin.ModelAdmin):
+    fields = ("content", "is_relevant")
+    list_display = ("id", "pretty_content", "date_created", "date_updated", "is_relevant")
+    list_display_links = ("id",)
+    ordering = ("-date_updated",)
+    list_filter = ("is_relevant",)
+
+    def pretty_content(self, obj):
+        return mark_safe(f"<div style='border: 1px solid var(--hairline-color);'> {obj.content} </div>")
+
+    pretty_content.short_description = DocHeader._meta.get_field("content").verbose_name
+
+    def delete_queryset(self, request, qs):
+        if qs.filter(is_relevant=True):
+            qs_remaining = DocHeader.objects.order_by("-date_updated").exclude(pk__in=qs.values("pk"))
+            if qs_remaining:
+                relevant = qs_remaining[0]
+                relevant.is_relevant = True
+                relevant.save()
+        qs.delete()
+
+
+@admin.register(Instruction)
+class InstructionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PartTitle)
+class PartTitleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Part)
+class PartAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AccessRight)
+class AccessRightAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(HistoryLog)
+class HistoryLogAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.site_title = "RESHUFFLE"
 admin.site.site_header = "RESHUFFLE"
--- reshuffle/main/models.py
+++ reshuffle/main/models.py
@@ -1,3 +1,74 @@
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import Group
+
+from ckeditor.fields import RichTextField
+
+
+class AbstractDatestamp(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name=_("Date created"))
+    date_updated = models.DateTimeField(auto_now=True, verbose_name=_("Date updated"))
+
+    class Meta:
+        abstract = True
+
+
+class Subject(AbstractDatestamp):
+    pass
+
+
+class Difficulty(AbstractDatestamp):
+    pass
+
+
+class Task(AbstractDatestamp):
+    pass
+
+
+class Option(AbstractDatestamp):
+    pass
+
+
+class DocHeader(AbstractDatestamp):
+    content = RichTextField(config_name="Config_DocHeader", verbose_name=_("Content"))
+    is_relevant = models.BooleanField(default=True, verbose_name=_("Relevant"))
+
+    def save(self, *args, **kwargs):
+        if self.is_relevant:
+            type(self).objects.all().update(is_relevant=False)
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        qs = type(self).objects.order_by("-date_updated")
+        qs = qs.exclude(pk=self.pk)
+        if qs:
+            qs.filter(pk=qs[0].pk).update(is_relevant=True)
+        super().delete(*args, **kwargs)
+
+    def __str__(self):
+        state = _("Relevant") if self.is_relevant else _("Irrelevant")
+        return f"ID: {self.id} ({state})"
+
+    class Meta:
+        verbose_name = _("Document header")
+        verbose_name_plural = _("Document headers")
+
+
+class Instruction(AbstractDatestamp):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+
+class PartTitle(AbstractDatestamp):
+    pass
+
+
+class Part(AbstractDatestamp):
+    pass
+
+
+class AccessRight(AbstractDatestamp):
+    pass
+
+
+class HistoryLog(AbstractDatestamp):
+    pass
