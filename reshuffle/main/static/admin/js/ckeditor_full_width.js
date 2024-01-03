eaab566581313efeafb9e508d863b781380a0413
r4nd0lph <r11345roma@yandex.ru>
2024-01-03 20:53:02 +0300

    fixed unnecessary code activation

--- reshuffle/main/static/admin/js/ckeditor_full_width.js
+++ reshuffle/main/static/admin/js/ckeditor_full_width.js
@@ -1,5 +1,8 @@
 window.onload = () => {
-    let textarea = document.getElementById("id_content");
-    let container = textarea.parentElement.parentElement;
-    container.classList = null;
+    const state = window.location.pathname.split("/").at(-2);
+    if (state === "add" || state === "change") {
+        let textarea = document.getElementById("id_content");
+        let container = textarea.parentElement.parentElement;
+        container.classList = null;
+    }
 };
