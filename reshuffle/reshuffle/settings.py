57c35f9cfba30143ded86da85e23653f1770a06c
r4nd0lph <r11345roma@yandex.ru>
2024-01-04 15:46:22 +0300

    added support for changing styles for admin panel

--- reshuffle/reshuffle/settings.py
+++ reshuffle/reshuffle/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "reshuffle.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [Path(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
