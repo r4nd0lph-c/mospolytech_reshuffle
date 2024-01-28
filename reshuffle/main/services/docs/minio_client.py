ae655fae0a351187c16e9d5c61027d4503b8a8ad
r4nd0lph <r11345roma@yandex.ru>
2024-01-28 12:50:03 +0300

    added delete_object() func for MinioClient (renamed)

--- reshuffle/main/services/docs/minio_client.py
+++ reshuffle/main/services/docs/minio_client.py
@@ -4,21 +4,21 @@ from minio import Minio
 from reshuffle.settings import MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_BUCKET_NAME
 
 
-class MinioUploader:
+class MinioClient:
     """
     ...
     """
 
     def __init__(self) -> None:
-        self.client = Minio(
+        self.__client = Minio(
             endpoint=MINIO_ENDPOINT,
             access_key=MINIO_ACCESS_KEY,
             secret_key=MINIO_SECRET_KEY,
             secure=False
         )
-        self.bucket_name = MINIO_BUCKET_NAME
-        if not self.client.bucket_exists(self.bucket_name):
-            self.client.make_bucket(self.bucket_name)
+        self.__bucket_name = MINIO_BUCKET_NAME
+        if not self.__client.bucket_exists(self.__bucket_name):
+            self.__client.make_bucket(self.__bucket_name)
 
     def upload_folder(self, folder: str, alias: str = None) -> None:
         if not alias:
@@ -27,8 +27,8 @@ class MinioUploader:
             if not path.isfile(f):
                 self.upload_folder(f, path.join(alias, f.split("\\")[-1]))
             else:
-                self.client.fput_object(
-                    bucket_name=self.bucket_name,
+                self.__client.fput_object(
+                    bucket_name=self.__bucket_name,
                     object_name=path.join(alias, f.split("\\")[-1]).replace("\\", "/"),
                     file_path=f
                 )
@@ -36,7 +36,12 @@ class MinioUploader:
     def upload_file(self, file: str, alias: str = None) -> None:
         if not alias:
             alias = file.split("\\")[-1]
-        self.client.fput_object(bucket_name=self.bucket_name, object_name=alias, file_path=file)
+        self.__client.fput_object(bucket_name=self.__bucket_name, object_name=alias, file_path=file)
+
+    def delete_object(self, alias: str) -> None:
+        alias += "/"
+        for o in self.__client.list_objects(bucket_name=self.__bucket_name, prefix=alias, recursive=True):
+            self.__client.remove_object(bucket_name=self.__bucket_name, object_name=o.object_name)
 
 
 if __name__ == "__main__":
