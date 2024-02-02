2dde399f7250be0067b3705f557afb21a2fe2389
r4nd0lph <r11345roma@yandex.ru>
2024-02-02 18:08:11 +0300

    added find_mask() func for Analyzer

--- reshuffle/main/services/ocr/analyzer.py
+++ reshuffle/main/services/ocr/analyzer.py
@@ -21,11 +21,16 @@ class Analyzer:
     def grayscale(self, img: ndarray) -> ndarray:
         return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    def blur(self, img: ndarray, kernel: tuple = (5, 5), sigma: int = 1) -> ndarray:
+    def blur(self, img: ndarray, kernel: tuple = (5, 5), sigma: float = 1) -> ndarray:
         return cv2.GaussianBlur(img, kernel, sigma)
 
     def canny(self, img: ndarray, threshold1: float = 100, threshold2: float = 200) -> ndarray:
-        return cv2.Canny(self.blur(self.grayscale(img)), threshold1, threshold2)
+        if img.shape == 3:
+            img = self.grayscale(img)
+        return cv2.Canny(self.blur(img), threshold1, threshold2)
+
+    def threshold(self, img: ndarray, thresh: float = 85, maxval: float = 255) -> ndarray:
+        return cv2.threshold(img, thresh, maxval, cv2.THRESH_BINARY)[1]
 
     def dilate(self, img: ndarray, kernel: ndarray = np.ones((5, 5)), iterations: int = 1) -> ndarray:
         return cv2.dilate(img, kernel, iterations=iterations)
@@ -67,16 +72,39 @@ class Analyzer:
         matrix = cv2.getPerspectiveTransform(points_1, points_2)
         # restore perspective
         restored = cv2.warpPerspective(restored, matrix, (w, h))
+        # return result
         return restored
+
+    def find_mask(self, img: ndarray, l_1: int = 1, l_2: int = 40) -> ndarray:
+        # define vertical and horizontal kernels
+        kernel_1_h = np.ones((1, l_1), np.uint8)
+        kernel_1_v = np.ones((l_1, 1), np.uint8)
+        kernel_2_h = np.ones((1, l_2), np.uint8)
+        kernel_2_v = np.ones((l_2, 1), np.uint8)
+        # detect horizontal lines
+        img_h = cv2.morphologyEx(~img, cv2.MORPH_CLOSE, kernel_1_h)
+        img_h = cv2.morphologyEx(img_h, cv2.MORPH_OPEN, kernel_2_h)
+        # detect vertical lines
+        img_v = cv2.morphologyEx(~img, cv2.MORPH_CLOSE, kernel_1_v)
+        img_v = cv2.morphologyEx(img_v, cv2.MORPH_OPEN, kernel_2_v)
+        # merge lines
+        mask = self.dilate(img_h | img_v)
+        # return result
+        return self.threshold(mask, maxval=255)
 
 
 if __name__ == "__main__":
     a = Analyzer()
-    img_base = cv2.imread("test_3.png")
-    result = a.restore_perspective(img_base)
+    img_base = cv2.imread("test_1.png")
+    img_restore_perspective = a.restore_perspective(a.grayscale(img_base))
+    img_threshold = a.threshold(img_restore_perspective)
+    img_mask = a.find_mask(img_threshold)
 
     cv2.imshow("img_base", a.resize(img=img_base, k=4))
-    cv2.imshow("restore_perspective", a.resize(img=result, k=4))
+    cv2.imshow("threshold", a.resize(img=img_threshold, k=4))
+    cv2.imwrite("threshold.png", img_threshold)
+    cv2.imshow("mask", a.resize(img=img_mask, k=4))
+    cv2.imwrite("mask.png", img_mask)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
